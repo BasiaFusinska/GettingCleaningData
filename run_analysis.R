33f2a25dbc7507246d2c4fb75fa4d603fb580c24
bfusion <barbara.fusinska@gmail.com>
2018-02-11 13:01:02 +0000

    Moved all the code to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,37 @@
+## ---- Read the data ---
+
+# Read data from training and testing dataset
+trainFile <- "data/train/X_train.txt"
+testFile <- "data/test/X_test.txt"
+
+trainData <- read.csv(trainFile, header=FALSE, sep="")
+testData <- read.csv(testFile, header=FALSE, sep="")
+
+# Read features
+featuresFile <- "data/features.txt"
+features <- read.csv(featuresFile, header = FALSE, sep="")
+
+# Read the activity labels
+activityLabelsFile <- "data/activity_labels.txt"
+activityLabels <- read.csv(activityLabelsFile, header = FALSE, sep = "")
+names(activityLabels) <- c("Id", "Name")
+
+# Read the data labels
+trainLabelsFile <- "data/train/y_train.txt"
+testLabelsFile <- "data/test/y_test.txt"
+
+trainLabels <- read.csv(trainLabelsFile, header = FALSE)
+testLabels <- read.csv(testLabelsFile, header = FALSE)
+
+# Read subjects
+subjectsTrainFile <- "data/train/subject_train.txt"
+subjectsTestFile <- "data/test/subject_test.txt"
+
+trainSubjects <- read.csv(subjectsTrainFile, header = FALSE)
+testSubjects <- read.csv(subjectsTestFile, header = FALSE)
+
+
+## ---- Run the analysis ---
 
 # Merge both training and testing datasets
 allData <- rbind(trainData, testData)
@@ -26,3 +60,7 @@ msData$subject <- allSubjects$subject
 
 # Group data for each activity and subject
 groupedData <- aggregate(. ~ activity + subject, msData, mean)
+
+## ---- Save the dataset to the file ---
+groupedDataFile <- "data/cleanData.txt"
+write.table(groupedData, groupedDataFile, row.names = FALSE)
--- save_data.R
+++
@@ -1,3 +0,0 @@
-# Save data to the file
-groupedDataFile <- "data/cleanData.txt"
-write.table(groupedData, groupedDataFile, row.names = FALSE)
